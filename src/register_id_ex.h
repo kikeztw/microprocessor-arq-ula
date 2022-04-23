8562c6574fbbf2e27d4b460b709b4cd8d1e31b76
kikeztw <kikeztw@gmail.com>
2022-04-23 19:05:36 -0400

    fix if/ex file

--- src/register_id_ex.h
+++ src/register_id_ex.h
@@ -11,16 +11,16 @@ public:
   // clock
   sc_in<bool> clkIn;
   // input
-  sc_in<string> rRg1In, rRg2In;
-  sc_in<sc_uint<32>> immIn, immIn2,cpIn;
+  sc_in<string> tagIn;
+  sc_in<sc_uint<32>> immIn, immIn2,cpIn, rRg1In, rRg2In;
   // output
-  sc_out<sc_uint<32>> cpOut, immOut, immOut2;
-  sc_out<string> rd1Out, rd2Out;
+  sc_out<sc_uint<32>> cpOut, immOut, immOut2, rd1Out, rd2Out;
+  sc_out<string> tagOut;
 
   SC_CTOR(RegisterIFEX);
 private:
-  std::string rRg1Store, rRg2InStore;
-  sc_uint<32> cpStore, immStore, immStore2;
+  std::string tagStore;
+  sc_uint<32> cpStore, immStore, immStore2, rRg1Store, rRg2InStore;
   void read();
   void write();
 };
