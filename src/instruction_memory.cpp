a2d9ab09682eb815163448dd285cdbc176045446
Fabian Flores <famercado24@gmail.com>
2022-04-23 18:21:40 -0400

    Unidad de control, pero hay un problema

--- src/instruction_memory.cpp
+++ src/instruction_memory.cpp
@@ -19,7 +19,7 @@ void InstructioMemory::read() {
   auto addres = address.read();
   std::string block;
 
-  if(addres >= L1_I.size())
+  if(addres >= (L1_I.size() + 1))
     sc_stop();
 
   // busca el bloque si no esta, no devuelve nada
