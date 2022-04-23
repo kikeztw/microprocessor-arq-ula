ec34cd8ea420087c6ef72cecdefda2ffcc04f360
Fabian Flores <famercado24@gmail.com>
2022-04-23 19:06:32 -0400

    conexiones parte 1

--- src/register_id_ex.h
+++ src/register_id_ex.h
@@ -11,11 +11,11 @@ public:
   // clock
   sc_in<bool> clkIn;
   // input
-  sc_in<string> rRg1In, rRg2In;
-  sc_in<sc_uint<32>> immIn, immIn2,cpIn;
+  sc_in<sc_int<32>> rRg1In, rRg2In;
+  sc_in<sc_int<32>> immIn, immIn2,cpIn;
   // output
-  sc_out<sc_uint<32>> cpOut, immOut, immOut2;
-  sc_out<string> rd1Out, rd2Out;
+  sc_out<sc_int<32>> cpOut, immOut, immOut2;
+  sc_out<sc_int<32>> rd1Out, rd2Out;
 
   SC_CTOR(RegisterIFEX);
 private:
