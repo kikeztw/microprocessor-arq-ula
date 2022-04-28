2e3c16fc728c0882fcbfb513d19a8bc232b1919d
Fabian Flores <famercado24@gmail.com>
2022-04-28 19:27:39 -0400

    erro que se solciono con make clean

--- src/data_memory.cpp
+++ src/data_memory.cpp
@@ -4,13 +4,6 @@ DataMemory::DataMemory(sc_module_name nm) : sc_module(nm) {
   L1_D.reserve(100);
   SC_METHOD(op)
     sensitive << opCodeIn;
-
-  L1_D.push_back(56);
-  L1_D.push_back(2);
-  L1_D.push_back(50);
-  L1_D.push_back(423);
-  L1_D.push_back(15);
-  
   
 }
 
@@ -23,10 +16,10 @@ void DataMemory::op() {
   
    if (op_code == LW) {
     // lee
-    // valueOut.write(L1_D[addressIn_]);
+    valueOut.write(L1_D[addressIn_]);
   } else if (op_code == SW) {
     // escribe
-    //L1_D[addressIn_] = valueIn.read();
+    L1_D[addressIn_] = valueIn.read();
   } 
 }
 
