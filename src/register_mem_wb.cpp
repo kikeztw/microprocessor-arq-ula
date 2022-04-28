fb4cd01e66ae4a1bb0d3e3ec0861e562b2433ae8
kikeztw <kikeztw@gmail.com>
2022-04-28 15:24:00 -0400

    finish registerMEMWB

--- src/register_mem_wb.cpp
+++ src/register_mem_wb.cpp
@@ -10,8 +10,11 @@ RegisterMEMWB::RegisterMEMWB(sc_module_name nm) : sc_module(nm) {
 }
 
 void RegisterMEMWB::read() {
+  regDestinyOut.write(regDestinyStore);
+  rdOut.write(dataStore);
 }
 
 void RegisterMEMWB::write() {
-
+  dataStore = rdIn.read();
+  regDestinyStore = regDestinyIn.read();
 }
