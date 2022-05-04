bc7e856499739b5d6f70b471ea3245f7445270f0
frankz19 <frankzerpa19@gmail.com>
2022-05-04 19:21:47 -0400

    fix bug test

--- src/ejemplo03.asm
+++ src/ejemplo03.asm
@@ -2,25 +2,10 @@
   v: 12,21,23
 .text
   main:
-    lw x2, 2(0)
-    add x4, x0, 3
-    add x3, x0, 5
-    add x7, x0, 1
-    add x0, x0, x0
-    add x0, x0, x0
-    add x0, x0, x0
+    add x5, x0, 20
   while:
-    sw x3, 1(x7)
-    sub x5, x4, x3
-    beq x0, x0, while
-    add x8, x2, x3
-    add x0, x0, x0
-    add x0, x0, x0
-    sw x5, 1(x0)
-
-
-
-
-
-
-
+    add x3, x3, 1
+    add x0, x0, 0
+    add x0, x0, 0
+    add x0, x0, 0
+    bne x3, x5, while
